ddc470425880581b2e5c46bc68f5b04c71700c20
baimiyishu13 <baimiyishu13@163.com>
2024-02-03 19:03:37 +0800

    Added an executeTemplate function to render

--- study_code/lenslocked/main.go
+++ study_code/lenslocked/main.go
@@ -7,14 +7,15 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
-// 基本的 web 应用程序
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 解析 - 执行 gohtml文件
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles("./templates/home.gohtml")
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("Parsing template: %v", err)
 		http.Error(w, "The wars an error parsing the template", http.StatusInternalServerError)
@@ -28,15 +29,21 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// 另一个 界面
-func cantactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Cantact Page</h1> \n<p>To get in touch: \n<a href=\"https://github.com/baimiyishu13\">github</a>\n</p>\n<p> ")
+// 基本的 web 应用程序
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, "./templates/home.gohtml")
 }
 
+// 另一个 界面
+func cantactHandler(w http.ResponseWriter, r *http.Request) {
+	// executeTemplate(w, "./templates/contact.gohtml")
+	tplpath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplpath)
+}
+
+// FQA 界面
 func fqaHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1> FQA </h1>")
+	executeTemplate(w, "./templates/fqa.gohtml")
 }
 
 // Gin
